43bd7340a41ea50795c47f9c07b5f0f72d65bc88
Akhil Achary <0018akhil@gmail.com>
2020-10-22 16:00:31 +0530

    Added sessions
    Login & logout are added

--- home.php
+++ home.php
@@ -2,6 +2,12 @@
 session_start();
 $username = $_SESSION['username'];
 echo "<h1>Hello, " . $username . ".</h1>";
+
+if(isset($_REQUEST['logout'])){
+    session_unset();
+    session_destroy();
+    echo "<script> location.href='login.php' </script>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,9 +24,22 @@ echo "<h1>Hello, " . $username . ".</h1>";
             font-size: 100px;
             text-transform: capitalize;
         }
+        input{
+            height: 50px;
+            width: 100px;
+            background-color: lightskyblue;
+            color: white;
+            font-size: x-large;
+            border-radius: 10px;
+            font-family: monospace;
+        }
+        input:hover{
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
     <p>Have a nice day.</p>
+    <input type="button" name="logout" id="logout" value="logout">
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,69 +1,73 @@
 <?php
+if (!isset($_SESSION['username'])) {
+    $db_server = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_base = "test";
 
-$db_server = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_base = "test";
-
-try {
-    $conn = mysqli_connect($db_server, $db_username, $db_password, $db_base);
-    // echo "Connected";
-} catch (Exception $e) {
-    echo "error in database";
-}
-
-function check($conn)
-{
-    $sel = "SELECT * FROM form;";
-    $result = mysqli_query($conn, $sel);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            if ($row['email'] == $_REQUEST['email']) {
-                return true;
-            }
-        }
-    } else {
-        return false;
+    try {
+        $conn = mysqli_connect($db_server, $db_username, $db_password, $db_base);
+        // echo "Connected";
+    } catch (Exception $e) {
+        echo "error in database";
     }
-}
 
-function pass($conn)
-{
-    $sel = "SELECT * FROM form;";
-    $result = mysqli_query($conn, $sel);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            if (password_verify($_REQUEST['password'], $row['password'])) {
-                return true;
-            }
-        }
-    } else {
-        return false;
-    }
-}
-
-if (isset($_POST['submit'])) {
-
-
-    if (($_REQUEST['email'] == "") || ($_REQUEST['password'] == "")) {
-        echo "All fields are required";
-    } else {
-        if (check($conn)) {
-
-            if (pass($conn)) {
-                $stmt = "SELECT name FROM form WHERE email ='" . $_REQUEST['email']. "';";
-                $res = mysqli_query($conn, $stmt);
-                $out = mysqli_fetch_assoc($res);
-                session_start();
-                $_SESSION['username'] = $out['name'];
-                header("Location:home.php");
-            } else {
-                echo "Password is wrong";
+    function check($conn)
+    {
+        $sel = "SELECT * FROM form;";
+        $result = mysqli_query($conn, $sel);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($row['email'] == $_REQUEST['email']) {
+                    return true;
+                }
             }
         } else {
-            echo "No such user";
+            return false;
         }
     }
+
+    function pass($conn)
+    {
+        $sel = "SELECT * FROM form;";
+        $result = mysqli_query($conn, $sel);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                if (password_verify($_REQUEST['password'], $row['password'])) {
+                    return true;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+    if (isset($_POST['submit'])) {
+
+
+        if (($_REQUEST['email'] == "") || ($_REQUEST['password'] == "")) {
+            echo "All fields are required";
+        } else {
+            if (check($conn)) {
+
+                if (pass($conn)) {
+                    $stmt = "SELECT name FROM form WHERE email ='" . $_REQUEST['email'] . "';";
+                    $res = mysqli_query($conn, $stmt);
+                    $out = mysqli_fetch_assoc($res);
+                    session_start();
+                    $_SESSION['username'] = $out['name'];
+                    echo "<script>location.href='./home.php'</script>";
+                } else {
+                    echo "Password is wrong";
+                }
+            } else {
+                echo "No such user";
+            }
+        }
+    }
+}
+else{
+    echo "<script>location.href='./home.php'</script>";
 }
 
 ?>
@@ -76,6 +80,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="stylesign.css">
+    
 </head>
 
 <body>
